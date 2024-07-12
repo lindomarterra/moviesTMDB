acaf23ef1b7c36fd42d8923f0cab30b709cb5fb6
Terra <terra.lindomar@gmail.com>
2024-07-12 20:14:07 -0300

    commit z-index

--- src/components/arrowup.jsx
+++ src/components/arrowup.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Arrowup = () => {
   return (
-    <div className="bg-white position-fixed bottom-0 rounded-5 d-flex align-self-center m-3">
+    <div className="arrow-up bg-white position-fixed bottom-0 rounded-5 d-flex align-self-center m-3">
       <Link to="/" target="_top" className="text-decoration-none px-2 py-2">
         <abbr title="go to home page!">
           <BiArrowToTop className="text-black fs-4"/>
